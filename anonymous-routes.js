6ce72bdb5849279f565c22e59f5e63b9e93f342f
Michael Colenso <github@michaelcolenso.com>
2015-11-29 20:22:14 -0800

    lots of changes

--- anonymous-routes.js
+++ anonymous-routes.js
@@ -1,8 +1,13 @@
-var express = require('express'),
-    quoter  = require('./quoter');
+var express = require( "express" ),
+    quoter  = require( "./quoter" );
 
 var app = module.exports = express.Router();
 
-app.get('/api/random-quote', function(req, res) {
-  res.status(200).send(quoter.getRandomOne());
-});
+app.get( "/api/random-quote", function( req, res ) {
+  res.status( 200 ).send( quoter.getRandomOne() );
+} );
+
+app.post( "/outbound", function( req, res ) {
+  res.type( "text/xml" );
+  res.render( "outbound" );
+} );
